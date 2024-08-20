26d79b97fe7799501f345ffb1613685801049a2d
SL Dilla Gaming <71542535+DillaEditz@users.noreply.github.com>
2024-08-21 02:05:35 +0530

    Create System.js

---
+++ plugins/System.js
@@ -0,0 +1,23 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const os = require("os")
+// Register the 'System' command
+cmd({
+    pattern: "system",
+    alias: ["status","botinfo"],
+    desc: "Check if the bot is online.",
+    category: "main",
+    filename: __filename
+}, async (conn, mek, m, {
+    from, quoted, body, isCmd, command, args, q, isGroup,
+    sender, senderNumber, botNumber2, botNumber, pushname, isMe,
+    isOwner, groupMetadata, groupName, participants, groupAdmins,
+    isBotAdmins, isAdmins, reply
+}) => {
+    try {
+
+    }catch(e){
+      console.log(e)
+      reply(`${e}`)
+    }
+    })
