4161dd2ac8ce274451c1fc1c3cb7d6a6121ee089
SL Dilla Gaming <71542535+DillaEditz@users.noreply.github.com>
2024-08-21 01:12:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,4 +15,5 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'FqUmTRYB#OW4kriCtKEJhn_M6q_z2O8ilbblJQ2Jsv0_BQ_sL0po',
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3653d1cd025076c0559d5.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "I am Chuti_Yakshani-MD Created By Dilla Editz !",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
