776634e1b08807ef6b1d24c11054ea4de2d2cf79
SL Dilla Gaming <71542535+DillaEditz@users.noreply.github.com>
2024-08-20 22:55:48 +0530

    Update Song+video.js

--- plugins/Song+video.js
+++ plugins/Song+video.js
@@ -1,1 +1,23 @@
+const { cmd, commands } = require('../command');
+const fg = require('api-dylux')
+const yts = require('yt-search')
 
+// Register the 'alive' command
+cmd({
+    pattern: "song",
+    desc: "download song",
+    category: "download",
+    filename: __filename
+}, async (conn, mek, m, {
+    from, quoted, body, isCmd, command, args, q, isGroup,
+    sender, senderNumber, botNumber2, botNumber, pushname, isMe,
+    isOwner, groupMetadata, groupName, participants, groupAdmins,
+    isBotAdmins, isAdmins, reply
+}) => {
+    try {
+
+    }catch(e){
+      console.log(e)
+      reply('${e}')
+    }
+})
