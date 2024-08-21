dc53791938335c331a7337ac61f9e0d48bcf8e19
SL Dilla Gaming <71542535+DillaEditz@users.noreply.github.com>
2024-08-21 13:49:30 +0530

    Update System.js

--- plugins/System.js
+++ plugins/System.js
@@ -1,17 +1,7 @@
 const config = require('../config');
 const { cmd, commands } = require('../command');
 const os = require("os");
-
-// Function to format uptime
-function runtime(seconds) {
-    seconds = Number(seconds);
-    const d = Math.floor(seconds / (3600 * 24));
-    const h = Math.floor((seconds % (3600 * 24)) / 3600);
-    const m = Math.floor((seconds % 3600) / 60);
-    const s = Math.floor(seconds % 60);
-    
-    return `${d}d ${h}h ${m}m ${s}s`;
-}
+const { runtime } = require('../lib/functions');
 
 // Register the 'System' command
 cmd({
