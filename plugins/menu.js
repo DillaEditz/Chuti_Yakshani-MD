bfd5d85095bfe43b89a8b764e132e2a849398b45
MR DILLA <71542535+DillaEditz@users.noreply.github.com>
2024-08-23 00:11:35 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,1 +1,23 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
 
+// Register the 'menu' command
+cmd({
+    pattern: "menu",
+    alias: ["allmenu", "help"],
+    desc: "Restart the bot",
+    category: "main",
+    filename: __filename
+}, async (conn, mek, m, {
+    from, quoted, body, isCmd, command, args, q, isGroup,
+    sender, senderNumber, botNumber2, botNumber, pushname, isMe,
+    isOwner, groupMetadata, groupName, participants, groupAdmins,
+    isBotAdmins, isAdmins, reply
+}) => {
+    try {
+
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
