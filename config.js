1ddb1b9d05ef5f9892ff30208c8abeffe0c4fee5
SL Dilla Gaming <71542535+DillaEditz@users.noreply.github.com>
2024-08-22 14:49:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ function convertToBool(text, fault = 'true') {
 
 // Export the SESSION_ID environment variable
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'FqUmTRYB#OW4kriCtKEJhn_M6q_z2O8ilbblJQ2Jsv0_BQ_sL0po',
+    SESSION_ID: process.env.SESSION_ID || 'NiNkiLYR#s9WY9HeYnG9-epInW_DW3IcjtJff8HdUGmYUO3SkfRM',
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3653d1cd025076c0559d5.jpg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "I am Chuti_Yakshani-MD Created By Dilla Editz !",
+    ALIVE_MSG: process.env.ALIVE_MSG || "*Type .menu For The Menu.*",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     MODE: process.env.MODE || "public",
 };
