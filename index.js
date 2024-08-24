80ce70e5021e0938aef0bd0bc68197f3945e5a1d
MR DILLA <71542535+DillaEditz@users.noreply.github.com>
2024-08-24 13:36:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
+
+    //=============connect MONGODB=========
+    const connectDB = require('./lib/mongodb')
+    connectDB();
+    //================================
     console.log("Connecting Chuti_Yakshani-MD 🧬...");
     const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/');
     var { version } = await fetchLatestBaileysVersion();
