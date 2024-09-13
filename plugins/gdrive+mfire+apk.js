8883e38785cf764890e974085d9b9d11546c6989
MR DILLA <dilukahettiarachchi3@gmail.com>
2024-09-13 21:10:08 +0530

    Update gdrive+mfire+apk.js

--- plugins/gdrive+mfire+apk.js
+++ plugins/gdrive+mfire+apk.js
@@ -50,3 +50,24 @@ cmd({ pattern: "mediafire",
     reply(`Error: ${e.message}`);
   }
 });
+
+cmd({
+    pattern: "apk",
+    alias: ["modapk"],
+    desc: "download apks",
+    category: "download",
+    react: "⚡",
+    filename: __filename
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q && !q.startsWith("https://")) return reply("❗Apk Not Found,Sorry")
+        //fetch data from api  
+        let data = await fetchJson(`${baseUrl}/api/apkdl?url=${q}`)
+        reply("*plase waite...*")
+        await conn.sendMessage(from, { document: { url: data.data.link_1 }, fileName: data.data.name, mimetype: data.data.file_type, caption: cap }, { quoted: mek })                                                                                                                 
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
